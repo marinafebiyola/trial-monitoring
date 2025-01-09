1017f2c300655181efa719c9329a7e9743a619b3
marinafebiyola <marinafebiyol@gmail.com>
2025-01-09 16:41:05 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,15 @@ from datetime import datetime
 
 st.set_page_config(page_title="monitoring-kki-2024", page_icon="🌍", layout="wide")
 
+st.markdown("""
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+    <style>
+        .sidebar-text, .header-text, .judul-text {
+            font-family: 'Roboto', sans-serif;
+        }
+    </style>
+    """, unsafe_allow_html=True)
+
 # Back4App credentials
 BASE_URL    = "https://parseapi.back4app.com/classes/Monitoring"
 headers     = {
